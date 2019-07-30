33187e99f7ba2bfb5fa9fbd0550531b9f5ac1d22
Urs Voegele <unext@bluewin.ch>
2019-07-30 14:13:06 +0200

    box moved to own repo

---
+++ README.md
@@ -0,0 +1,8 @@
+### ubuntu-18.04-rke
+Vagrant Box with Ubuntu 18.04
+
+#### Base image
+Used base image [elegoev/ubuntu-18.04](https://app.vagrantup.com/elegoev/boxes/ubuntu-18.04)
+
+#### Automatic provisioning
+The base image is provisioned with bash script [ubuntu-18.04-rke.sh](https://github.com/elegoev/vagrant-ubuntu/blob/master/vagrant-ubuntu1804-rke/provisioning/ubuntu-18.04-rke.sh)
---
+++ provisioning/ubuntu-18.04-rke.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# set variables
+RKE_VERSION="0.1.18"
+RKE_FILENAME="rke_linux-amd64"
+RKE_BINDIR="/usr/local/bin"
+
+# get rke image
+sudo wget -q -O /tmp/$RKE_FILENAME https://github.com/rancher/rke/releases/download/v$RKE_VERSION/$RKE_FILENAME
+sudo mv /tmp/$RKE_FILENAME $RKE_BINDIR/rke
+sudo chmod +x $RKE_BINDIR/rke
+
+# create date string
+DATE=`date +%Y%m%d%H%M`
+
+# get rke version
+RKE_INST_VERSION=$(rke --version | awk  '{print $3}' | tr --delete v)
+echo "$RKE_INST_VERSION.$DATE" > /vagrant/version

62ee3be96593d554ef17d330862ec0b0da5b36ef
Urs Voegele <unext@bluewin.ch>
2021-02-22 15:14:45 +0100

    new basebox version 2.0.1614002770

--- installed-application.md
+++ installed-application.md
@@ -1,6 +1,6 @@
 # Installed application 
 ***                     
 > Docker: 19.03.15
-> Kubectl: 1.20.2
+> Kubectl: 1.20.4
 > Helm: 3.5.2
 > rke: 1.2.5
--- packer/provisioners/base-provisioning.sh
+++ packer/provisioners/base-provisioning.sh
@@ -1,22 +1,69 @@
 #!/bin/bash
 
+if [ -n "$1" ]; then
+  box_name=""
+else
+  box_name=$1
+fi
+
 application_file_path="/vagrant/installed-application.md"
+rancher_cluster_dir="/home/vagrant/rancher-rke-cluster"
 
 # set variables
-RKE_VERSION="1.2.5"
-RKE_FILENAME="rke_linux-amd64"
-RKE_BINDIR="/usr/local/bin"
+rke_version="1.2.5"
+rke_filename="rke_linux-amd64"
+rke_bin_dir="/usr/local/bin"
 
 # get rke image
-sudo wget -q -O /tmp/$RKE_FILENAME https://github.com/rancher/rke/releases/download/v$RKE_VERSION/$RKE_FILENAME
-sudo mv /tmp/$RKE_FILENAME $RKE_BINDIR/rke
-sudo chmod +x $RKE_BINDIR/rke
+sudo wget -q -O /tmp/$rke_filename https://github.com/rancher/rke/releases/download/v$rke_version/$rke_filename
+sudo mv /tmp/$rke_filename $rke_bin_dir/rke
+sudo chmod +x $rke_bin_dir/rke
 
 # install kubectl
 sudo snap install kubectl --classic
 
 # install helm
 sudo snap install helm --classic
+
+# create rancher cluster directory
+sudo mkdir ${rancher_cluster_dir}
+sudo chown vagrant:vagrant ${rancher_cluster_dir}
+
+# copy cluste config
+sudo cp /home/vagrant/files-prov/rke/rancher-cluster.yml ${rancher_cluster_dir}/rancher-cluster.yml
+sudo chown vagrant:vagrant ${rancher_cluster_dir}/rancher-cluster.yml
+
+# copy private key
+echo ">>>>> boxname = ${box_name}"
+local_ssh_path="/home/vagrant/.ssh"
+ssh_source_key="/vagrant/.vagrant/machines/${box_name}/virtualbox/private_key"
+ssh_privkey_filename="id_rsa"
+ssh_authorized_key="authorized_keys"
+sudo cp $ssh_source_key $local_ssh_path/$ssh_privkey_filename
+sudo chown vagrant:vagrant $local_ssh_path/$ssh_privkey_filename
+sudo chmod 600 $local_ssh_path/$ssh_privkey_filename
+sudo chown vagrant:vagrant $local_ssh_path/$SSHPRIVKEYFILENAME
+sudo cat $local_ssh_path/$ssh_privkey_filename >> $local_ssh_path/$ssh_authorized_key
+
+# copy .ssh for root
+sudo cp -r $local_ssh_path /root
+
+# create cluster
+cd ${rancher_cluster_dir}
+rke up --config ./rancher-cluster.yml
+sudo chown vagrant:vagrant *
+
+# copy kube config to $HOME/.kube/config
+kube_config_dir="/home/vagrant/.kube"
+if [ ! -d "$kube_config_dir" ]; then
+  sudo mkdir -p $kube_config_dir
+fi
+sudo cp kube_config_rancher-cluster.yml $kube_config_dir/config
+sudo chown vagrant:vagrant $kube_config_dir/config
+sudo chmod 0600 $kube_config_dir/config
+
+# copy .kube for root
+sudo cp -r $kube_config_dir /root
 
 # set version
 DOCKER_VERSION=$(sudo docker version --format '{{.Server.Version}}')
